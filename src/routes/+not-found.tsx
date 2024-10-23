2fd931d9c360dc889d83c64696bdce7fbd60d21b
batukaraman <batuhankaraman0@hotmail.com>
2024-10-24 02:06:34 +0300

    React Native project started with Expo (TypeScript, router, customized path)

---
+++ src/routes/+not-found.tsx
@@ -0,0 +1,24 @@
+import { Link, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View style={styles.container}>
+        <Text>This screen doesn't exist.</Text>
+        <Link href="/">
+          <Text>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
